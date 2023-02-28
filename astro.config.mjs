64f436020c7f83e90ea84d160f532e68a6448740
aarthificial <aarthificial@gmail.com>
2023-03-01 00:08:20 +0100

    build: deploy to GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import {defineConfig} from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 
 export default defineConfig({
+  site: 'https://aarthificial.com',
   integrations: [solidJs()],
 });
